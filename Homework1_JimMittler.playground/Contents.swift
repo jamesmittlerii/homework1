319d27d9e79851fb9dd96ca9341f7dee89eebc9f
Jim Mittler <chicagozer@gmail.com>
2025-08-19 15:41:11 -0700

    Initial Commit

---
+++ Homework1_JimMittler.playground/Contents.swift
@@ -0,0 +1,16 @@
+/**
+ 
+ * Homework 1
+ * Jim Mittler
+ * 13 August 2025
+ 
+ _Italic text_
+ __Bold text__
+ ~~Strikethrough text~~
+
+ */
+
+import Cocoa
+
+var greeting = "Hello, from Jim Mittler"
+print(greeting)
